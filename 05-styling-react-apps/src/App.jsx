43a4e1cb1ebb9ab79cfb4ee716d7cecee5c1f803
Selma Saltık <selmasaltiik@gmail.com>
2024-07-11 05:40:20 +0300

    Add files via upload
    Styling with Tailwind CSS
    * Adding & Using Tailwind CSS In A React Project
    * Tailwind: Media Queries & Pseudo Selectors
    * Dynamic & Conditional Styling with Tailwind

---
+++ 05-styling-react-apps/src/App.jsx
@@ -0,0 +1,13 @@
+import Header from './components/Header.jsx';
+import AuthInputs from './components/AuthInputs.jsx';
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <AuthInputs />
+      </main>
+    </>
+  );
+}
